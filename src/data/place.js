4a95f8ba0735a50e10dadf3eb9d034f4ff6267c1
David Dlp <dlpdavid17@gmail.com>
2022-01-15 17:34:48 +0100

    static data

---
+++ src/data/place.js
@@ -0,0 +1,29 @@
+const placeData = [
+    {
+        title: "",
+        image: "" ,
+        description: "",
+        link: "",
+    },
+    {
+        title: "",
+        image: "",
+        description: "",
+        link: "",
+    },
+    {
+        title: "",
+        image: "",
+        description: "",
+        link: "",
+    },
+    {
+        title: "",
+        image: "",
+        description: "",
+        link: "",
+    },
+    
+];
+
+export default placeData;
